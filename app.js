5c4615e841c1d1578ed0e9ae210d4d08b47f823c
dan850 <dan06982@gmail.com>
2022-11-18 00:34:01 -0600

    update

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const circle = document.querySelector('#circle');
 
+const circle2 = document.querySelector('#circle2');
+
+
+const closedFace = document.querySelector('.closed');
+const openFace = document.querySelector('.open');
+
+
+closedFace.addEventListener('mouseenter', () => {
+    if (openFace.classList.contains('open')) {
+      openFace.classList.add('active');
+      closedFace.classList.remove('active');
+    }
+  });
+
+
 circle.addEventListener('mouseenter', () => {
   if (!circle.classList.contains('hover')) {
     circle.classList.add('hover');
@@ -11,3 +26,16 @@ circle.addEventListener('mouseleave', () => {
     circle.classList.remove('hover');
   }
 });
+
+
+circle2.addEventListener('mouseenter', () => {
+    if (!circle2.classList.contains('hover2')) {
+      circle2.classList.add('hover2');
+    }
+  });
+  
+  circle2.addEventListener('mouseleave', () => {
+    if (circle2.classList.contains('hover2')) {
+      circle2.classList.remove('hover2');
+    }
+  });
