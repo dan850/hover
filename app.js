4616e9000eb6ec655054cdc68365f288e4201d6a
dan850 <dan06982@gmail.com>
2022-11-18 20:41:49 -0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
-const circle = document.querySelector('#circle');
+const circle = document.querySelector('.circle');
 
-const circle2 = document.querySelector('#circle2');
+const circle2 = document.querySelector('.circle2');
 
 
 const closedFace = document.querySelector('.closed');
 const openFace = document.querySelector('.open');
+const closedFace2 = document.querySelector('.closed2');
+const openFace2 = document.querySelector('.open2');
 
 
-closedFace.addEventListener('mouseenter', () => {
-    if (openFace.classList.contains('open')) {
-      openFace.classList.add('active');
-      closedFace.classList.remove('active');
+closedFace.addEventListener('click', () => {
+  if (openFace.classList.contains('open')) {
+    openFace.classList.add('active');
+    closedFace.classList.remove('active');
+    circle.classList.add('hover');
+
+  }
+
+});
+
+openFace.addEventListener('click', () => {
+  if (closedFace.classList.contains('closed')) {
+    closedFace.classList.add('active');
+    openFace.classList.remove('active');
+  }
+});
+
+  
+
+closedFace2.addEventListener('click', () => {
+    if (openFace2.classList.contains('open2')) {
+      openFace2.classList.add('active2');
+      closedFace2.classList.remove('active2');
     }
+
   });
+  openFace2.addEventListener('click', () => {
+    if (closedFace2.classList.contains('closed2')) {
+      closedFace2.classList.add('active2');
+      openFace2.classList.remove('active2');
+      
+    }
+
+}); 
 
 
 circle.addEventListener('mouseenter', () => {
